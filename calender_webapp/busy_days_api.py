96d5f82a81d99140acb0f590588f49b37cc12894
Romanec Tatyana <romanectatyana@gmail.com>
2025-06-25 13:05:06 +0300

    1

--- calender_webapp/busy_days_api.py
+++ calender_webapp/busy_days_api.py
@@ -272,4 +272,4 @@ def submit_appointment():
         return jsonify({"status": "error", "message": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
