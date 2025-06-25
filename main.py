fbb39ebecb949754231e8992f6aab40f9c6ce7e7
Romanec Tatyana <romanectatyana@gmail.com>
2025-06-25 15:33:08 +0300

    1

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ from pytz import timezone
 import bot.keyboards.menu as menu
 from bot.handlers import services, reviewes, start, appoints, single_reviewes, master_appoint, master_timetable, master_notes, master_bloked_time
 from bot.database.connection import get_pool, create_pool
-import asyncio
+import threading
+
+# Импортируем твой Flask app:
+from calender_webapp.busy_days_api import app as flask_app
 
 load_dotenv()
 
@@ -25,7 +28,7 @@ async def fallback_handler(message: Message):
         "Я Бот і не вмію відповідати на текстові повідомлення. Ось головне меню 👇",
         reply_markup=menu.main_menu_with_cancel
     )
-    
+
 async def update_appointments_job():
     while True:
         print("🔄 Запуск оновлення записів...")
@@ -40,11 +43,18 @@ async def update_appointments_job():
             ''')
             print("✅ Оновлення завершено.")
         
-        await asyncio.sleep(43200)  # 12 часов в секундах (12 * 60 * 60)
-        
-            
+        await asyncio.sleep(43200)  # 12 годин
+
+# функция для запуска Flask в фоне:
+def run_flask():
+    flask_app.run(host='0.0.0.0', port=5000)
+
 async def main():
-    await  create_pool()
+    # Запускаем Flask app в отдельном потоке:
+    flask_thread = threading.Thread(target=run_flask)
+    flask_thread.start()
+
+    await create_pool()
     asyncio.create_task(update_appointments_job())
     dp.include_router(master_appoint.router)
     dp.include_router(master_timetable.router)
@@ -55,10 +65,10 @@ async def main():
     dp.include_router(start.router)
     dp.include_router(appoints.router)
     dp.include_router(single_reviewes.router)
-    dp.include_router(router) 
+    dp.include_router(router)
+
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
-    
-    
+
 if __name__ == "__main__":
     asyncio.run(main())
